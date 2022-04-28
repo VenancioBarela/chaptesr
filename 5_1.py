348d96f55c311f1e40e6fe23f48644f42bd0a36e
Venancio Barela <calivee.ca@gmail.com>
2022-04-28 15:26:29 -0700

    third commit

--- 5_1.py
+++ 5_1.py
@@ -30,23 +30,60 @@
 # # main()
 
 
-# create file object in write mode
+# # create file object in write mode
 
-def main(:)
-    save_to_outfile("./outfile.txt")
+# def main(:)
+#     save_to_outfile("./outfile.txt")
 
-def open_file(file_name,mode):
-    return open(file_name, mode)
+# def open_file(file_name,mode):
+#     return open(file_name, mode)
 
-outfile = open_file("./outfile.txt", "w")
+# outfile = open_file("./outfile.txt", "w")
 
+# def save_to_outfile(file_name):
+# # for name in names:
+# #     if "Doe" not in name:
+#         print("Name to be persisted:", name)
+#         outfile.write(name)
+#         outfile.write(name + "\n")
+#         data.appen(name)
+#     else:
+#         print("Names to be excluded:", name)
+# outfile.close()
 
-for name in names:
-    if "Doe" not in name:
-        print("Name to be persisted:", name)
-        outfile.write(name)
-        outfile.write(name + "\n")
-        data.appen(name)
-    else:
-        print("Names to be excluded:", name)
-outfile.close()
+# a_list = []
+
+# a_set{}
+
+# a_list.append ("A")
+
+# a_set.add("A")
+# a.set.add("A")
+# a set.add("B")
+# a set.add("C")
+
+# def main():
+#     words = ["nudge", "nudge", "wink", "wink"]
+
+#     terms = set(words)
+    
+#     print("terms", terms)
+#     terms.add("wink")
+#     terms.add("new wink")
+#     terms.discard("nudge")
+
+# main ()
+
+arr1 = ["Alpha", "Bravo" "Charlie"]
+arr2 = ["Bravo", "Dalta"]
+set1 = set(arr1)
+set2 = set(arr2)
+
+print("set1", set1)
+print("set12", set2)
+
+# Built-in set metthods: 1 .union, 2 .intersection, 3 .difference()
+
+print("union:",set1.union(set2))
+print("intersection",set1.intersection(set2))
+print("difference",set1.difference(set2))
