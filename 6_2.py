95fbe7f4e9dd8e99173d17da63e32ca5cc473794
Venancio Barela <calivee.ca@gmail.com>
2022-05-10 14:43:45 -0700

    second commit

---
+++ 6_2.py
@@ -0,0 +1,30 @@
+# from random import sample
+
+
+# elements = [
+# "earth",
+# "air",
+# "fire",
+# "water"]
+
+# taking an item out of list randomly
+#  print("choice====>",random.choice elements
+# takes a sample out of original list
+#  print(sample====>,random.sample elements)
+# shuffles ordering of items withing list
+# # print("shuffle====>",elements)
+# returns a number from 1-5
+# print("randint====>",random.int(0,5))
+# chapter 6 (6.2) example 4
+# def main():
+    
+#     for i in range(3):
+#         print("i",1)
+#         outcome = spin()
+#         print(outcome,end)
+#         spin()
+
+# def spin():
+#     m = random.randint(1,20)
+#     print("random number generated:",n)
+#     if n > 15:
